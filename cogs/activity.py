b36b209ee4e9a691c8999526c27a3aa034aa3c0c
Begginersz <47064053+Begginersz@users.noreply.github.com>
2019-05-12 09:43:49 +0600

    Add files via upload

---
+++ cogs/activity.py
@@ -0,0 +1,370 @@
+# -*- coding: utf-8 -*-
+
+"""
+The MIT License (MIT)
+
+Copyright (c) 2017 SML
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
+
+import argparse
+import datetime as dt
+import os
+from collections import Counter
+
+import aiohttp
+import discord
+from cogs.utils import checks
+from cogs.utils.chat_formatting import box
+from cogs.utils.dataIO import dataIO
+from discord.ext import commands
+from discord.ext.commands import Context
+from tinydb import Query
+from tinydb import TinyDB
+from tinydb.storages import JSONStorage
+from tinydb_serialization import SerializationMiddleware
+from tinydb_serialization import Serializer
+
+PATH_LIST = ['data', 'activity']
+PATH = os.path.join(*PATH_LIST)
+JSON = os.path.join(*PATH_LIST, "settings.json")
+DB = os.path.join(*PATH_LIST, "db.json")
+HOST = '127.0.0.1'
+INTERVAL = 5
+
+
+class DateTimeSerializer(Serializer):
+    OBJ_CLASS = dt.datetime  # The class this serializer handles
+
+    def encode(self, obj):
+        return obj.strftime('%Y-%m-%dT%H:%M:%S')
+
+    def decode(self, s):
+        return dt.datetime.strptime(s, '%Y-%m-%dT%H:%M:%S')
+
+
+serialization = SerializationMiddleware(JSONStorage)
+serialization.register_serializer(DateTimeSerializer(), 'TinyDate')
+
+
+class Activity:
+    """Activity Logger.
+
+    Logs activity of a Discord server.
+    Displays:
+    - Most active user by message sent
+    - Richest user via bank economy module
+    - Server Stats
+
+    Settings
+    - server_id
+      - year, week number
+        - messages
+        - commands
+        - mentions
+        - message_time
+      - on_off
+      - server_id
+      - server_name
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.settings = dataIO.load_json(JSON)
+        self.session = aiohttp.ClientSession(loop=self.bot.loop)
+        self.rank_max = 5
+        self.db = TinyDB(
+            DB, storage=serialization, default_table='messages',
+            sort_keys=True, indent=4, separators=(',', ': ')
+        )
+        self.table_settings = self.db.table('settings')
+        self.table_messages = self.db.table('messages')
+
+    def __unload(self):
+        self.lock = True
+        self.session.close()
+
+    def save_json(self):
+        """Save settings."""
+        dataIO.save_json(JSON, self.settings)
+
+    @commands.group(pass_context=True)
+    @checks.mod_or_permissions()
+    async def activityset(self, ctx: Context):
+        """Change activity logging settings."""
+        if ctx.invoked_subcommand is None:
+            await self.bot.send_cmd_help(ctx)
+
+    @activityset.command(name="toggle", pass_context=True, no_pm=True)
+    @checks.mod_or_permissions()
+    async def as_toggle(self, ctx):
+        """Toggle server monitoring."""
+        server_id = ctx.message.server.id
+
+        Settings = Query()
+        db = self.db.table('settings')
+        results = db.search(Settings.server_id == server_id)
+
+        if len(results) == 0:
+            on_off = True
+        else:
+            r = results[0]
+            on_off = not r['on_off']
+
+        db.upsert({'server_id': server_id, 'on_off': on_off}, Settings.server_id == server_id)
+
+        await self.bot.say("Monitor server activity: {}".format(on_off))
+
+
+
+    def parser(self, cat):
+        """Argument parser."""
+        parser = argparse.ArgumentParser(prog='[p]activity')
+        parser.add_argument(
+            '-r', '--roles',
+            nargs='+',
+            help='Include roles')
+        parser.add_argument(
+            '-t', '--top',
+            nargs=1,
+            help='Top N results',
+            type=int,
+            default=10
+        )
+        parser.add_argument(
+            '-d', '--days',
+            nargs=1,
+            help='Last N days',
+            type=int,
+            default=7
+        )
+        return parser
+
+    def output_str(self, title, mc_list, server=None, limit=10, prop=None):
+        """
+        output str
+        :param title:
+        :param mc_list:
+        :param prop:
+        :return:
+        """
+        if prop is None:
+            return ''
+        if server is None:
+            return ''
+
+        out = []
+        for c in mc_list[:limit]:
+            name = ''
+            if prop == 'channel':
+                name = server.get_channel(c[0]).name
+            elif prop == 'member':
+                member = server.get_member(c[0])
+                if member is None:
+                    name = 'User {}'.format(c[0])
+                else:
+                    name = member.display_name
+            out.append('{:<20.20}: {:>5}'.format(name, c[1]))
+
+        s = '\n'.join([title, box('\n'.join(out), lang='py')])
+        return s
+
+    @commands.group(pass_context=True, aliases=['act'])
+    async def activity(self, ctx):
+        """Activity."""
+        if ctx.invoked_subcommand is None:
+            await self.bot.send_cmd_help(ctx)
+
+    @activity.command(name="user", aliases=['u'], pass_context=True, no_pm=True)
+    async def a_user(self, ctx, member: discord.Member = None, *args):
+        """User activity."""
+        await self.bot.type()
+
+        parser = self.parser('channel')
+        try:
+            pargs = parser.parse_args(args)
+        except SystemExit:
+            await self.bot.send_cmd_help(ctx)
+            return
+        limit = pargs.top
+        days = pargs.days
+
+        server = ctx.message.server
+        author = ctx.message.author
+
+        if member is None:
+            member = author
+
+        from_date = dt.datetime.utcnow() - dt.timedelta(days=days)
+
+        Msg = Query()
+
+        results = self.db.search(
+            (Msg.server_id == server.id)
+            & (Msg.author_id == member.id)
+            & (Msg.timestamp >= from_date)
+        )
+        channel_ids = [r['channel_id'] for r in results]
+        channel_id_mc = Counter(channel_ids).most_common()
+
+        await self.bot.say(
+            self.output_str(
+                'Channel activity for {}, top {}, last {} days'.format(author, limit, days),
+                channel_id_mc,
+                server=server, limit=limit, prop='channel'
+            )
+        )
+
+    @activity.command(name="channel", aliases=['c'], pass_context=True, no_pm=True)
+    async def a_channel(self, ctx, channel: discord.Channel = None, *args):
+        """Channel activity."""
+        await self.bot.type()
+
+        parser = self.parser('channel')
+        try:
+            pargs = parser.parse_args(args)
+        except SystemExit:
+            await self.bot.send_cmd_help(ctx)
+            return
+        limit = pargs.top
+        days = pargs.days
+
+        server = ctx.message.server
+
+        if channel is None:
+            channel = ctx.message.channel
+
+        from_date = dt.datetime.utcnow() - dt.timedelta(days=days)
+
+        Msg = Query()
+        results = self.db.search(
+            (Msg.channel_id == channel.id)
+            & (Msg.timestamp >= from_date)
+        )
+        author_ids = [r['author_id'] for r in results]
+        author_id_mc = Counter(author_ids).most_common()
+
+        # Limit members by roles
+        mc = []
+        if pargs.roles:
+            for author_id, count in author_id_mc.copy():
+                member = server.get_member(author_id)
+                if member is not None:
+                    for role_name in pargs.roles:
+                        if role_name.lower() in [r.name.lower() for r in member.roles]:
+                            mc.append((author_id, count))
+                            break
+                # break out of loop if over required limit
+                if len(mc) > limit:
+                    break
+        else:
+            mc = author_id_mc.copy()
+
+
+        await self.bot.say(
+            self.output_str(
+                'User activity for {}, top {},  last {} days'.format(channel.mention, limit, days),
+                mc,
+                server=server, limit=limit, prop='member'
+            )
+        )
+
+    @activity.command(name="server", aliases=['s'], pass_context=True, no_pm=True)
+    async def a_server(self, ctx, *args):
+        """Server activity."""
+        await self.bot.type()
+
+        parser = self.parser('server')
+        try:
+            pargs = parser.parse_args(args)
+        except SystemExit:
+            await self.bot.send_cmd_help(ctx)
+            return
+        limit = pargs.top
+        days = pargs.days
+
+        server = ctx.message.server
+        from_date = dt.datetime.utcnow() - dt.timedelta(days=days)
+
+        Msg = Query()
+        results = self.db.search(
+            (Msg.server_id == server.id)
+            & (Msg.timestamp >= from_date)
+        )
+        authors = []
+        for r in results:
+            authors.append(r['author_id'])
+
+        mc_authors = Counter(authors).most_common(limit)
+
+        await self.bot.say(
+            self.output_str(
+                'Server activity for {}, top {}, last {} days'.format(server, limit, days),
+                mc_authors,
+                server=server, limit=limit, prop='member'
+            )
+        )
+
+    async def on_message(self, message: discord.Message):
+        """Log number of messages."""
+        server = message.server
+        author = message.author
+        channel = message.channel
+
+        if server is None:
+            return
+
+        if channel is None:
+            return
+
+        Settings = Query()
+        db = self.table_settings
+        r = db.get(Settings.server_id == server.id)
+
+        if r is not None:
+            if not r['on_off']:
+                return
+
+        self.table_messages.insert({
+            'author_id': author.id,
+            'server_id': server.id,
+            'channel_id': channel.id,
+            'message_content': message.content,
+            'timestamp': dt.datetime.utcnow(),
+            'bot': author.bot
+        })
+
+
+def check_folders():
+    if not os.path.exists(PATH):
+        os.mkdir(PATH)
+
+
+def check_files():
+    if not dataIO.is_valid_json(JSON):
+        defaults = {}
+        dataIO.save_json(JSON, defaults)
+
+
+def setup(bot):
+    check_folders()
+    check_files()
+    n = Activity(bot)
+    bot.add_cog(n)
---
+++ cogs/stats.py
@@ -0,0 +1,206 @@
+import discord
+from discord.ext import commands
+from cogs.utils import checks
+from .utils.dataIO import dataIO, fileIO
+from __main__ import send_cmd_help
+import os
+import datetime
+from time import time as get_time
+import asyncio
+
+settings_path = "data/stats/settings.json"
+
+
+class stats:
+    """Discord user stats in a voice channel"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.settings = dataIO.load_json(settings_path)
+        self.member_log = dataIO.load_json('data/clanlog/member_log.json')
+        self.discord_log = dataIO.load_json('data/clanlog/discord_log.json')
+        self.last_count = {}
+        self.cycle_task1 = bot.loop.create_task(self.updateUserCount())
+        self.cycle_task2 = bot.loop.create_task(self.updateMiscCount())
+
+    def __unload(self):
+        self.cycle_task1.cancel()
+        self.cycle_task2.cancel()
+
+    @commands.group(pass_context=True, no_pm=True)
+    @checks.is_owner()
+    async def stats(self, ctx):
+        """Server Stats settings"""
+
+        if ctx.invoked_subcommand is None:
+            await send_cmd_help(ctx)
+
+    @stats.command(pass_context=True)
+    async def setup(self, ctx):
+        """Setup the server stats"""
+
+        server = ctx.message.server
+
+        everyone = discord.PermissionOverwrite(connect=False, view_channel=True)
+
+        member_channel = await self.bot.create_channel(server, '0 Members',
+                                                       (server.default_role, everyone),
+                                                       type=discord.ChannelType.voice)
+
+        guests_channel = await self.bot.create_channel(server, '0 Guests',
+                                                       (server.default_role, everyone),
+                                                       type=discord.ChannelType.voice)
+
+        online_channel = await self.bot.create_channel(server, '0 Online Users',
+                                                       (server.default_role, everyone),
+                                                       type=discord.ChannelType.voice)
+
+        user_channel = await self.bot.create_channel(server, '0 Total Users',
+                                                     (server.default_role, everyone),
+                                                     type=discord.ChannelType.voice)
+
+        server_channel = await self.bot.create_channel(server, '0 Days Old',
+                                                       (server.default_role, everyone),
+                                                       type=discord.ChannelType.voice)
+
+        time_channel = await self.bot.create_channel(server, '0 GMT',
+                                                     (server.default_role, everyone),
+                                                     type=discord.ChannelType.voice)
+        self.settings[server.id] = {}
+        self.settings[server.id]['channels'] = {}
+        self.settings[server.id]['channels']['member_channel'] = member_channel.id
+        self.settings[server.id]['channels']['guests_channel'] = guests_channel.id
+        self.settings[server.id]['channels']['online_channel'] = online_channel.id
+        self.settings[server.id]['channels']['user_channel'] = user_channel.id
+        self.settings[server.id]['channels']['server_channel'] = server_channel.id
+        self.settings[server.id]['channels']['time_channel'] = time_channel.id
+
+        fileIO(settings_path, "save", self.settings)
+        await self.bot.say("Stats Setup Compeleted.")
+
+    async def getUserCount(self, server, name):
+        """Returns the numbers of people with the member role"""
+        members = server.members
+
+        count = 0
+        for member in members:
+            for role in member.roles:
+                if role.name == name:
+                    count += 1
+
+        return count
+
+    async def updateUserCount(self):
+        """Update counter every few minutes"""
+        await self.bot.wait_until_ready()
+        try:
+            await asyncio.sleep(20)  # Start-up Time
+            while True:
+                servers = [x for x in self.bot.servers if x.id in self.settings]
+                for server in servers:
+                    channels = self.settings[server.id]['channels']
+                    values = self.last_count
+
+                    if server.id not in values:
+                        values[server.id] = {'member': 0, 'guests': 0, 'user': 0, 'server': 0}
+
+                    if server.id == "515502772926414933":
+                        self.member_log = dataIO.load_json('data/clanlog/bs_member_log.json')
+                        members = self.member_log[max(self.member_log.keys())]
+                    else:
+                        self.member_log = dataIO.load_json('data/clanlog/member_log.json')
+                        members = self.member_log[max(self.member_log.keys())]
+
+                    if members != values[server.id]['member']:
+                        values[server.id]['member'] = members
+                        await self.bot.edit_channel(server.get_channel(channels['member_channel']),
+                                                    name="{} Members".format(members))
+
+                    guests = await self.getUserCount(server, "Guest")
+                    if guests != values[server.id]['guests']:
+                        values[server.id]['guests'] = guests
+                        await self.bot.edit_channel(server.get_channel(channels['guests_channel']),
+                                                    name="{} Guests".format(guests))
+
+                    userTotal = len(server.members)
+                    if userTotal != values[server.id]['user']:
+                        current_time = get_time()
+
+                        if server.id not in self.discord_log:
+                            self.discord_log[server.id] = {}
+
+                        self.discord_log[server.id][str(current_time)] = userTotal
+                        dataIO.save_json('data/clanlog/discord_log.json', self.discord_log)
+                        values[server.id]['user'] = userTotal
+
+                        saved_times = list(self.discord_log[server.id].keys())
+                        for time in saved_times:
+                            if (current_time - float(time)) > 2678400:  # one month
+                                self.discord_log[server.id].pop(time, None)
+                        dataIO.save_json('data/clanlog/discord_log.json', self.discord_log)
+
+                        await self.bot.edit_channel(server.get_channel(channels['user_channel']),
+                                                    name="{} Total Users".format(userTotal))
+
+                    passed = (datetime.datetime.utcnow() - server.created_at).days
+                    if passed != values[server.id]['server']:
+                        values[server.id]['server'] = passed
+                        await self.bot.edit_channel(server.get_channel(channels['server_channel']),
+                                                    name="{} Days Old".format(passed))
+
+                await asyncio.sleep(600)  # task runs every 600 seconds
+        except asyncio.CancelledError:
+            pass
+
+    async def updateMiscCount(self):
+        """Update counter every few minutes"""
+        await self.bot.wait_until_ready()
+        try:
+            await asyncio.sleep(20)  # Start-up Time
+            while True:
+                servers = [x for x in self.bot.servers if x.id in self.settings]
+                for server in servers:
+                    channels = self.settings[server.id]['channels']
+
+                    online = len([m.status for m in server.members if m.status == discord.Status.online or m.status == discord.Status.idle])
+
+                    await self.bot.edit_channel(server.get_channel(channels['online_channel']),
+                                                name="{} Online Users".format(str(online)))
+                    await self.bot.edit_channel(server.get_channel(channels['time_channel']),
+                                                name="{} GMT".format(datetime.datetime.now(datetime.timezone.utc).strftime("%H:%M")))
+
+                await asyncio.sleep(15)  # task runs every 15 seconds
+        except asyncio.CancelledError:
+            pass
+
+
+def check_folders():
+    if not os.path.exists("data/stats"):
+        print("Creating data/stats folder...")
+        os.makedirs("data/stats")
+    if not os.path.exists("data/clanlog"):
+        print("Creating data/clanlog folder...")
+        os.makedirs("data/clanlog")
+
+
+def check_files():
+    f = settings_path
+    if not fileIO(f, "check"):
+        print("Creating stats settings.json...")
+        fileIO(f, "save", {})
+
+    f = "data/clanlog/member_log.json"
+    if not fileIO(f, "check"):
+        print("Creating empty member_log.json...")
+        fileIO(f, "save", {"1524540132": 0})
+
+    f = "data/clanlog/discord_log.json"
+    if not fileIO(f, "check"):
+        print("Creating empty discord_log.json...")
+        fileIO(f, "save", {"1524540132": 0})
+
+
+def setup(bot):
+    check_folders()
+    check_files()
+    bot.add_cog(stats(bot))
